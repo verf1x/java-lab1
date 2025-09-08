c68159d96b345f041424dbfd763012426ae56b33
Nikita Kuptsov <82285281+verf1x@users.noreply.github.com>
2025-09-08 08:23:27 +0400

    Play with command line args

--- MyFirstClass.java
+++ MyFirstClass.java
@@ -1,5 +1,8 @@
 public class MyFirstClass {
     public static void main(String[] args) {
-        System.out.println("Купцов");
+        if(args.length != 0)
+            for (String letter : args) {
+                System.out.println(letter);
+        }
     }
 }
